28af10f6f40f973511aa6324413c3bbea41152f1
umangtDesai <42987983+umangtDesai@users.noreply.github.com>
2019-01-02 15:55:37 +0530

    Update BUHacksArduinoToAPI.py

--- BUHacksArduinoToAPI.py
+++ BUHacksArduinoToAPI.py
@@ -3,12 +3,12 @@ from twilio.rest import Client
 
 def send_sms():
         
-        account_sid = "ACfaaab20da8f96fe6cb94d01dadada913"
-        auth_token = "5af38c68728c425ddf12b2855041a5f3"
+        account_sid = "**************************"
+        auth_token = "****************************"
         client = Client(account_sid, auth_token)
 
-        message = client.api.account.messages.create(to="+16468230989",
-                                             from_="(646) 679-6314",
+        message = client.api.account.messages.create(to="************",
+                                             from_="**********************",
                                              body="Someone's trying to enter your room!")
 
 
